8e6b8336661fbc47aedaf7f05fcf58e5df780dfa
KATHIRESHP <kathireshpalanisamy055@gmail.com>
2023-04-08 20:57:45 +0530

    205708042023

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Gamepage from './Components/Gamepage'
 export default function App() {
   return (
     <div>
-      <center className='bg-dark vh-20 display-6 text-light p-5 rounded'>Kathir's min-games</center>
+      <center className='bg-dark vh-20 display-6 text-light p-5 rounded position-fixed vw-100'>Kathir's min-games</center>
       <Gamepage />
     </div>
   )
--- src/Components/Gamepage.jsx
+++ src/Components/Gamepage.jsx
@@ -1,4 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function Gamepage() {
 
@@ -21,14 +24,14 @@ function Gamepage() {
 
     const btnHandler = (index) => {
         const tempArray = [...box];
-        if (isX == "X" && tempArray[index] == "_") {
+        if (isX === "X" && tempArray[index] === "_") {
             tempArray[index] = isX;
             let plyTempArray = [...player1, index];
             setPlayer1(plyTempArray);
             setIsX("o");
             setPlayer(2)
         }
-        else if (tempArray[index] == "_" && isX == "o") {
+        else if (tempArray[index] === "_" && isX === "o") {
             tempArray[index] = isX;
             let plyTempArray = [...player2, index];
             setPlayer2(plyTempArray);
@@ -52,44 +55,61 @@ function Gamepage() {
             const [x, y, z] = winningCombinations[i];
             if (player1.includes(x) && player1.includes(y) && player1.includes(z)) {
                 setWinner(1);
+                toast.success("Player 1 victory !!");
+                setTimeout(() => {
+                    resetHandler();
+                }, 5000)
             }
             if (player2.includes(x) && player2.includes(y) && player2.includes(z)) {
                 setWinner(2);
+                toast.success("Player 2 victory !!");
+                setTimeout(() => {
+                    resetHandler();
+                }, 5000)
             }
+        }
+        if((player1.length + player2.length) == 9 && winner === "")
+        {
+            console.log("Called....");
+            toast.warning("Match draw, \nResetting game...", {
+                autoClose: 2000
+            })
+            setTimeout(() => {
+                resetHandler();
+            }, 2000)
         }
     }, [player1, player2])
 
 
 
     return (
-        <div className='container d-flex flex-column vh-80 vw-100 justify-content-center align-items-center'>
-            {winner == "" &&
+        <div className='container d-flex flex-column vh-100 vw-100 justify-content-center align-items-center'>
+        <ToastContainer/>
+            {winner === "" &&
                 <div className='display-6 m-5'>Player {player}'s turn</div>
             }
-            <></>
-            {winner != "" &&
-                    <div className='display-6 fw-bolder'> Player {winner} won the Match</div>
-            }
-            <></>
-            {winner == "" &&
+            {winner === "" &&
                 <div className="border border-5 rounded p-3 border-dark bg-dark">
                     <div>
-                        <button className='btn btn-lg btn-success m-1' onClick={() => btnHandler(0)}>{box[0]}</button>
-                        <button className='btn btn-lg btn-success m-1' onClick={() => btnHandler(1)}>{box[1]}</button>
-                        <button className='btn btn-lg btn-success m-1' onClick={() => btnHandler(2)}>{box[2]}</button>
+                        <button className='btn btn-lg btn-success m-1 px-4' onClick={() => btnHandler(0)}>{box[0]}</button>
+                        <button className='btn btn-lg btn-success m-1 px-4' onClick={() => btnHandler(1)}>{box[1]}</button>
+                        <button className='btn btn-lg btn-success m-1 px-4' onClick={() => btnHandler(2)}>{box[2]}</button>
                     </div>
                     <div className=''>
-                        <button className='btn btn-lg btn-success m-1' onClick={() => btnHandler(3)}>{box[3]}</button>
-                        <button className='btn btn-lg btn-success m-1' onClick={() => btnHandler(4)}>{box[4]}</button>
-                        <button className='btn btn-lg btn-success m-1' onClick={() => btnHandler(5)}>{box[5]}</button>
+                        <button className='btn btn-lg btn-success m-1 px-4' onClick={() => btnHandler(3)}>{box[3]}</button>
+                        <button className='btn btn-lg btn-success m-1 px-4' onClick={() => btnHandler(4)}>{box[4]}</button>
+                        <button className='btn btn-lg btn-success m-1 px-4' onClick={() => btnHandler(5)}>{box[5]}</button>
                     </div>
                     <div className=''>
-                        <button className='btn btn-lg btn-success m-1' onClick={() => btnHandler(6)}>{box[6]}</button>
-                        <button className='btn btn-lg btn-success m-1' onClick={() => btnHandler(7)}>{box[7]}</button>
-                        <button className='btn btn-lg btn-success m-1' onClick={() => btnHandler(8)}>{box[8]}</button>
+                        <button className='btn btn-lg btn-success m-1 px-4' onClick={() => btnHandler(6)}>{box[6]}</button>
+                        <button className='btn btn-lg btn-success m-1 px-4' onClick={() => btnHandler(7)}>{box[7]}</button>
+                        <button className='btn btn-lg btn-success m-1 px-4' onClick={() => btnHandler(8)}>{box[8]}</button>
                     </div>
                 </div>
 
+            }
+            {winner !== "" &&
+                    <div className='display-6 fw-bolder'> Player {winner} won the Match</div>
             }
             <button className='btn btn-danger mt-5' onClick={() => resetHandler()}>Reset</button>
         </div>
